81310d31c71b74e9865bb6d40fffbb43a7767444
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-04-01 20:02:06 -0400

    feat: ensure AddProfessorController throw if AddProfessor throws

--- src/presentation/controllers/add-professor-controller.ts
+++ src/presentation/controllers/add-professor-controller.ts
@@ -1,6 +1,6 @@
 import type { AddProfessor } from 'domain/usecases/add-professor'
 import { MissingParamError } from '../errors'
-import { ok, badRequest } from '../helpers/http-helper'
+import { ok, badRequest, serverError } from '../helpers/http-helper'
 import type { Controller } from '../protocols/controller'
 import type { HttpRequest, HttpResponse } from '../protocols/http'
 
@@ -10,19 +10,23 @@ export class AddProfessorController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'email', 'tempoIc']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'tempoIc']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const professor = {
-      name: httpRequest.body.name,
-      email: httpRequest.body.email,
-      tempoIc: httpRequest.body.tempoIc
-    }
+      const professor = {
+        name: httpRequest.body.name,
+        email: httpRequest.body.email,
+        tempoIc: httpRequest.body.tempoIc
+      }
 
-    await this.addProfessor.add(professor)
-    return ok(httpRequest.body)
+      await this.addProfessor.add(professor)
+      return ok(httpRequest.body)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
