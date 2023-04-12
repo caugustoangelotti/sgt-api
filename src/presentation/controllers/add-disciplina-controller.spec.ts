b05b34d52627aeab47cd32e32b549d28ffef52ad
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-04-12 16:12:16 -0400

    test: ensure AddDisciplinaController throw if AddDisciplina throws

--- src/presentation/controllers/add-disciplina-controller.spec.ts
+++ src/presentation/controllers/add-disciplina-controller.spec.ts
@@ -1,8 +1,10 @@
+import { serverError } from '../helpers'
 import { ValidationSpy } from '../mocks'
 import { AddDisciplinaSpy } from '../mocks/mock-disciplina'
 import { AddDisciplinaController } from './add-disciplina-controller'
 
 import { randUuid, randNumber, randTextRange } from '@ngneat/falso'
+import MockDate from 'mockdate'
 
 interface SutTypes {
   sut: AddDisciplinaController
@@ -23,15 +25,44 @@ const makeSut = (): SutTypes => {
 
 const mockRequest = (): AddDisciplinaController.Request => ({
   name: randTextRange({ min: 40, max: 70 }),
-  semestre: randNumber(),
+  semestre: randNumber({ min: 1, max: 8 }),
   codigo: randUuid()
 })
 
 describe('Add Disciplina Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationSpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should call AddDisciplina with correct values', async () => {
+    const { sut, addDisciplinaSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    const response = {
+      ...request,
+      data_cadastro: new Date()
+    }
+    expect(addDisciplinaSpy.params).toEqual(response)
+  })
+
+  test('Should return 500 if AddDisciplina throws', async () => {
+    const { sut, addDisciplinaSpy } = makeSut()
+    const request = mockRequest()
+    jest.spyOn(addDisciplinaSpy, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
