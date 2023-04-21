1949accede47ef7786fbabe22f50a2544bdff69c
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-04-20 22:27:50 -0400

    test: ensure UpdateProfessorController checkByid and update professor with given information

--- src/presentation/controllers/update-professor-controller.spec.ts
+++ src/presentation/controllers/update-professor-controller.spec.ts
@@ -6,27 +6,31 @@
   * o retorno deve ser um professor com as informacoes atualizadas
 */
 
-import { noContent, ok, serverError } from '../helpers'
-import { UpdateProfessorSpy, ValidationSpy } from '../mocks'
+import { badRequest, ok, serverError } from '../helpers'
+import { CheckProfessorByIdSpy, UpdateProfessorSpy, ValidationSpy } from '../mocks'
 import { UpdateProfessorController } from './update-professor-controller'
 
 import MockDate from 'mockdate'
 import { randFullName, randUuid, randEmail } from '@ngneat/falso'
+import { InvalidParamError } from '../errors'
 
 interface SutTypes {
   sut: UpdateProfessorController
   updateProfessorSpy: UpdateProfessorSpy
   validationSpy: ValidationSpy
+  checkProfessorByIdSpy: CheckProfessorByIdSpy
 }
 
 const makeSut = (): SutTypes => {
   const updateProfessorSpy = new UpdateProfessorSpy()
   const validationSpy = new ValidationSpy()
-  const sut = new UpdateProfessorController(updateProfessorSpy, validationSpy)
+  const checkProfessorByIdSpy = new CheckProfessorByIdSpy()
+  const sut = new UpdateProfessorController(updateProfessorSpy, validationSpy, checkProfessorByIdSpy)
   return {
     sut,
     updateProfessorSpy,
-    validationSpy
+    validationSpy,
+    checkProfessorByIdSpy
   }
 }
 
@@ -52,6 +56,13 @@ describe('Update Professor Controller', () => {
     expect(validationSpy.input).toEqual(request)
   })
 
+  test('Should call CheckSurveyById with correct value', async () => {
+    const { sut, checkProfessorByIdSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(checkProfessorByIdSpy.id).toBe(request.id)
+  })
+
   test('Should call UpdateProfessor with correct values', async () => {
     const { sut, updateProfessorSpy } = makeSut()
     const request = mockRequest()
@@ -62,7 +73,7 @@ describe('Update Professor Controller', () => {
     expect(updateProfessorSpy.params).toEqual(response)
   })
 
-  test('Should return 500 if AddProfessor throws', async () => {
+  test('Should return 500 if UpdateProfessor throws', async () => {
     const { sut, updateProfessorSpy } = makeSut()
     const request = mockRequest()
     jest.spyOn(updateProfessorSpy, 'update').mockImplementationOnce(() => {
@@ -72,11 +83,31 @@ describe('Update Professor Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
-  test('Should return 204 if professor id not exists', async () => {
-    const { sut, updateProfessorSpy } = makeSut()
-    jest.spyOn(updateProfessorSpy, 'update').mockReturnValueOnce(Promise.resolve(false))
+  test('Should return 500 if CheckSurveyById throws', async () => {
+    const { sut, checkProfessorByIdSpy } = makeSut()
+    const request = mockRequest()
+    jest.spyOn(checkProfessorByIdSpy, 'checkById').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 500 if Validation throws', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRequest()
+    jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 400 if CheckSurveyById returns false', async () => {
+    const { sut, checkProfessorByIdSpy } = makeSut()
+    checkProfessorByIdSpy.result = false
     const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(noContent())
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('surveyId')))
   })
 
   test('Should return 200 and professor data on success', async () => {
