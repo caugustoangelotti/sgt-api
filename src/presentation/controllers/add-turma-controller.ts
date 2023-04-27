0aa56fbd5a182f489c9d9112857e2922d20ce687
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-04-27 02:03:47 -0400

    feat: ensure AddTurmaController throw if validation throws

--- src/presentation/controllers/add-turma-controller.ts
+++ src/presentation/controllers/add-turma-controller.ts
@@ -1,5 +1,5 @@
 import type { AddTurma } from '../../domain/usecases'
-import { badRequest } from '../helpers'
+import { badRequest, serverError } from '../helpers'
 import type { Controller, HttpResponse, Validation } from '../protocols'
 
 export class AddTurmaController implements Controller {
@@ -9,24 +9,28 @@ export class AddTurmaController implements Controller {
   ) {}
 
   async handle (request: any): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
-    if (error) {
-      return badRequest(error)
-    }
-    const turma = {
-      disciplina: request.disciplina,
-      horarios: request.horarios,
-      modelo: request.modelo,
-      semestre: request.semestre,
-      data_cadastro: new Date()
-    }
-    const fields = ['professor', 'tempo_ic']
-    for (const field of fields) {
-      if (request[field]) {
-        turma[field] = request[field]
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
       }
+      const turma = {
+        disciplina: request.disciplina,
+        horarios: request.horarios,
+        modelo: request.modelo,
+        semestre: request.semestre,
+        data_cadastro: new Date()
+      }
+      const fields = ['professor', 'tempo_ic']
+      for (const field of fields) {
+        if (request[field]) {
+          turma[field] = request[field]
+        }
+      }
+      await this.addTurma.add(turma)
+    } catch (error) {
+      return serverError(error)
     }
-    await this.addTurma.add(turma)
     return null as any
   }
 }
