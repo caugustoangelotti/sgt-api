b0e64af26b5bd8be6a86b6d47e569ec6b7b42a51
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-04 02:29:19 -0400

    feat: routes setup for loadDisciplinas

--- src/main/routes/disciplina-routes.ts
+++ src/main/routes/disciplina-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '../../main/adapters/express-route-adapter'
-import { makeAddDisciplinaController } from '../../main/factories/controllers'
+import { makeAddDisciplinaController, makeLoadDisciplinaController } from '../../main/factories/controllers'
 
 import type { Router } from 'express'
 import { cordenadorAuth } from '../middlewares'
 
 export default (router: Router): void => {
   router.post('/disciplina', cordenadorAuth, adaptRoute(makeAddDisciplinaController()))
+  router.get('/disciplina', cordenadorAuth, adaptRoute(makeLoadDisciplinaController()))
 }
