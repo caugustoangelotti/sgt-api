dff96258431b0e37036fc5f32d7ee14f28766c7b
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-16 15:25:46 -0400

    feat: add delete to professor routes

--- src/main/routes/professor-routes.ts
+++ src/main/routes/professor-routes.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '../../main/adapters/express-route-adapter'
-import { makeAddProfessorController, makeLoadProfessorController, makeUpdateProfessorController } from '../../main/factories/controllers'
+import { makeAddProfessorController, makeLoadProfessorController, makeRemoveProfessorController, makeUpdateProfessorController } from '../../main/factories/controllers'
 
 import type { Router } from 'express'
 
@@ -7,4 +7,5 @@ export default (router: Router): void => {
   router.post('/professor', adaptRoute(makeAddProfessorController()))
   router.get('/professores', adaptRoute(makeLoadProfessorController()))
   router.patch('/professor', adaptRoute(makeUpdateProfessorController()))
+  router.delete('/professor', adaptRoute(makeRemoveProfessorController()))
 }
