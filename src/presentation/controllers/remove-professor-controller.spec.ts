95e4bc80e970d5c39bb37aa324824bf6e3432c06
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-16 14:41:35 -0400

    test: ensure RemoveProfessorController throw if RemoveProfessor throws

--- src/presentation/controllers/remove-professor-controller.spec.ts
+++ src/presentation/controllers/remove-professor-controller.spec.ts
@@ -94,4 +94,14 @@ describe('Remove Professor Controller', () => {
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(serverError(new Error()))
   })
+
+  test('Should return 500 if UpdateProfessor throws', async () => {
+    const { sut, removeProfessorSpy } = makeSut()
+    const request = mockRequest()
+    jest.spyOn(removeProfessorSpy, 'remove').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
