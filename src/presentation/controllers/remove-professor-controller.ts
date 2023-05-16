a0dac6333d41f0d5ac7f25c514bb38c86b7b15f6
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-15 23:43:28 -0400

    feat: ensure RemoveProfessorController return 400 if validation fail

--- src/presentation/controllers/remove-professor-controller.ts
+++ src/presentation/controllers/remove-professor-controller.ts
@@ -1,4 +1,5 @@
 import type { CheckProfessorById, RemoveProfessor } from '../../domain/usecases'
+import { badRequest } from '../helpers'
 import type { Controller, HttpResponse, Validation } from '../protocols'
 
 export class RemoveProfessorController implements Controller {
@@ -11,11 +12,11 @@ export class RemoveProfessorController implements Controller {
   async handle (request: RemoveProfessorController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
-      return { statusCode: 400, body: error }
+      return badRequest(error)
     }
     const exists = await this.checkProfessorById.checkById(request.id)
     if (!exists) {
-      return { statusCode: 400, body: error }
+      return { statusCode: 200400, body: error }
     }
     await this.removeProfessor.remove(request.id)
     return await Promise.resolve({ statusCode: 200, body: {} })
