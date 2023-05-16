d9a63137fc8af52da6143cf8f7f555a5c9607ad0
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-15 23:30:56 -0400

    feat: ensure RemoveProfessorController call CheckProfessorById with correct values

--- src/presentation/controllers/remove-professor-controller.ts
+++ src/presentation/controllers/remove-professor-controller.ts
@@ -1,13 +1,20 @@
+import type { CheckProfessorById, RemoveProfessor } from '../../domain/usecases'
 import type { Controller, HttpResponse, Validation } from '../protocols'
 
 export class RemoveProfessorController implements Controller {
   constructor (
+    private readonly removeProfessor: RemoveProfessor,
+    private readonly checkProfessorById: CheckProfessorById,
     private readonly validation: Validation
   ) {}
 
   async handle (request: RemoveProfessorController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
+      return { statusCode: 400, body: error }
+    }
+    const exists = await this.checkProfessorById.checkById(request.id)
+    if (!exists) {
       return { statusCode: 400, body: error }
     }
     return await Promise.resolve({ statusCode: 200, body: {} })
