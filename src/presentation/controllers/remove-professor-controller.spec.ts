b365b93986f272f279ace4a0e2240881484c1e46
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2023-05-15 23:03:42 -0400

    test: ensure removeProfessorController calls validation with correct values

---
+++ src/presentation/controllers/remove-professor-controller.spec.ts
@@ -0,0 +1,46 @@
+import { CheckProfessorByIdSpy, RemoveProfessorSpy, ValidationSpy } from '../mocks'
+import { RemoveProfessorController } from './remove-professor-controller'
+
+import MockDate from 'mockdate'
+import { randNumber } from '@ngneat/falso'
+
+interface SutTypes {
+  sut: RemoveProfessorController
+  removeProfessorSpy: RemoveProfessorSpy
+  validationSpy: ValidationSpy
+  checkProfessorByIdSpy: CheckProfessorByIdSpy
+}
+
+const makeSut = (): SutTypes => {
+  const removeProfessorSpy = new RemoveProfessorSpy()
+  const validationSpy = new ValidationSpy()
+  const checkProfessorByIdSpy = new CheckProfessorByIdSpy()
+  const sut = new RemoveProfessorController(validationSpy)
+  return {
+    sut,
+    removeProfessorSpy,
+    validationSpy,
+    checkProfessorByIdSpy
+  }
+}
+
+const mockRequest = (): any => ({
+  id: randNumber({ min: 10000000, max: 99999999 })
+})
+
+describe('Remove Professor Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
+  })
+})
