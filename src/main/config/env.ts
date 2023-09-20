b0127162954e35fc1314480b35445c697ccf471e
caugustoangelotti <c.augustoangelotti@gmail.com>
2023-09-20 02:31:21 -0400

    fix: remove file

--- src/main/config/env.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/sgt-api',
-  postgresUrl: process.env.POSTGRES_URL ?? 'postgres://postgres:postgres@localhost:5432/sgt-api',
-  port: process.env.PORT ?? 5050,
-  jwtSecret: process.env.JWT_SECRET ?? 'tj67O==5H'
-}
